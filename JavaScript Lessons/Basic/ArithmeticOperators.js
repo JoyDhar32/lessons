a3cafe4769b8faf6b527d31ffc792a6e4dca934b
JoyDhar32 <joyustcian@gmail.com>
2024-01-19 12:32:44 +1100

    ⭐ In Depth

---
+++ JavaScript Lessons/Basic/ArithmeticOperators.js
@@ -0,0 +1,118 @@
+/*
+Addition (+):
+Adds two values.
+*/
+// Example
+var sum = 10 + 10;
+console.log(sum); // 20
+
+/* Subtraction (-):
+Subtracts one value from another.
+*/
+// Example
+var difference = 45 - 33;
+console.log(difference); // 12
+
+/* Multiplication (*):
+Multiplies two values.
+*/
+// Example
+var product = 8 * 10;
+console.log(product); // 80
+
+/* Division (/):
+Divides one value by another.
+*/
+// Example
+var quotient = 66 / 33;
+console.log(quotient); // 2
+
+/* Remainder (%):
+Returns the division remainder.
+*/
+// Example
+var remainder = 11 % 3;
+console.log(remainder); // 2
+
+/* Increment (++) and Decrement (--) Operators:
+Increment operator (++) increases the value of a variable by 1.
+Decrement operator (--) decreases the value of a variable by 1.
+*/
+// Example
+var myVar = 87;
+myVar++;
+console.log(myVar); // 88
+
+// Example
+var myVar = 11;
+myVar--;
+console.log(myVar); // 10
+
+/* Compound Assignment with Augmented Addition (+=):
+Adds a value to a variable.
+*/
+// Example
+var a = 3;
+var b = 17;
+var c = 12;
+a += 12;
+b += 9;
+c += 7;
+console.log(a); // 15
+console.log(b); // 26
+console.log(c); // 19
+
+/* Compound Assignment with Augmented Subtraction (-=):
+Subtracts a value from a variable.
+*/
+// Example
+var a = 11;
+var b = 9;
+var c = 3;
+a -= 6;
+b -= 15;
+c -= 1; 
+console.log(a); // 5
+console.log(b); // -6
+console.log(c); // 2
+
+/* Compound Assignment with Augmented Multiplication (*=):
+Multiplies a variable by a value.
+*/
+// Example
+var a = 5;
+var b = 12;
+var c = 4.6;
+a *= 5;
+b *= 3;
+c *= 10;
+console.log(a); // 25
+console.log(b); // 36
+console.log(c); // 46
+
+/* Compound Assignment with Augmented Division (/=):
+Divides a variable by a value.
+*/
+// Example
+var a = 48;
+var b = 108;
+var c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a); // 4
+console.log(b); // 27
+console.log(c); // 3
+
+/* Declare String Variables:
+Strings are used to store text.
+*/
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+console.log(firstName); // Alan
+console.log(lastName); // Turing
+
+
+
+
