539372e661624ce38671b74a847fad8bacc75b7b
JoyDhar32 <joyustcian@gmail.com>
2024-01-23 00:00:16 +1100

    ⭐  Math Functionalities

--- JS/Basic/Math.js
+++ JS/Basic/Math.js
@@ -36,7 +36,7 @@ Math.max(x,y,z,...): Returns the largest of zero or more numbers.
 console.log(`\nMath.max(x,y,z,...): Returns the largest of zero or more numbers.`);
 console.log(Math.max(1, 2, 3)); // 3
 const arrayy = [1, 2, 3,4,44,5];
-console.log(Math.max(...AnimationEffectarrayy)); // 44
+console.log(Math.max(...arrayy)); // 44
 console.log(arrayy); // [1, 2, 3]
 /*
 Math.min(x,y,z,...): Returns the smallest of zero or more numbers.
@@ -48,3 +48,59 @@ Math.pow(x,y): Returns base to the exponent power, that is, base exponent.
 console.log(`\nMath.pow(x,y): Returns base to the exponent power, that is, base exponent.`);
 console.log(Math.pow(2, 3)); // 8
 console.log(2 ** 3); // 8
+
+/*
+Math.sqrt(x): Returns the square root of a number.
+*/
+console.log(`\nMath.sqrt(x): Returns the square root of a number.`);
+console.log(Math.sqrt(9)); // 3
+console.log(Math.sqrt(16)); // 4
+console.log(Math.sqrt(25)); // 5
+
+/*
+Math.random(): Returns a pseudo-random number between 0 and 1.
+*/
+console.log(`\nMath.random(): Returns a pseudo-random number between 0 and 1.`);
+console.log(Math.random()); // 0.12345678912345678
+console.log(Math.random()); // 0.23423423423423423
+console.log(Math.floor(Math.random()*100)); // 0.34534534534534534
+
+/*
+Math.sign(x): Returns the sign of the x, indicating whether x is positive, negative or zero.
+*/
+console.log(`\nMath.sign(x): Returns the sign of the x, indicating whether x is positive, negative or zero.`);
+console.log(Math.sign(10)); // 1
+console.log(Math.sign(-10)); // -1
+console.log(Math.sign(0)); // 0
+console.log(Math.sign(90)); // 1
+
+/*
+Math.trunc(x): Returns the integer part of the number x, removing any fractional digits.
+*/
+console.log(`\nMath.trunc(x): Returns the integer part of the number x, removing any fractional digits.`);
+console.log(Math.trunc(1.1)); // 1
+console.log(Math.trunc(1.5)); // 1
+console.log(Math.trunc(1.9)); // 1
+console.log(Math.trunc(-1.1)); // -1
+console.log(Math.trunc(-5.5)); // -5
+
+/*
+Math.E: Returns Euler's number e (approx. 2.718).
+*/
+console.log(`\nMath.E: Returns Euler's number e (approx. 2.718).`);
+console.log(Math.E); // 2.718281828459045
+console.log(Math.E.toFixed(2)); // 2.72
+
+/*
+Math.LN2: Returns the natural logarithm of 2 (approx. 0.693).
+*/
+console.log(`\nMath.LN2: Returns the natural logarithm of 2 (approx. 0.693).`);
+console.log(Math.LN2); // 0.6931471805599453
+console.log(Math.LN2.toFixed(2)); // 0.69
+
+/*
+Math.LN10: Returns the natural logarithm of 10 (approx. 2.302).
+*/
+console.log(`\nMath.LN10: Returns the natural logarithm of 10 (approx. 2.302).`);
+console.log(Math.LN10); // 2.302585092994046
+console.log(Math.LN10.toFixed(2)); // 2.30
