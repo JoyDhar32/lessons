6c180efaecb4d1f887a946034962dc8aef5f7657
JoyDhar32 <joyustcian@gmail.com>
2024-01-22 17:06:17 +1100

    ⭐ JavsScript Lessons

---
+++ JS/Basic/Object.js
@@ -0,0 +1,22 @@
+/*
+In JavaScript, an object is a complex data type that allows you to store and organize data using key-value pairs. Objects are a fundamental part of the language, and they provide a flexible way to represent structured information. Here's an in-depth explanation of JavaScript objects:*/
+
+/*Creating Objects:
+Objects can be created using different syntaxes:*/
+
+// 1. Object Literal
+const person = {
+    firstName: "Joy",
+    lastName: "Dhar",
+    age: 25,
+    hobbies: ["Coding", "Playing Chess"],
+    address: {
+      city: "Sydney",
+      State: "NSW",
+      country: "Australia",
+    },
+  };
+  
+    console.log(person);
+    console.log(person.firstName);
+    console.log(person.lastName);
