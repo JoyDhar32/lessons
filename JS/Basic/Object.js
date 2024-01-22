07f54e809c65e9fe07b89297b68f835433839dea
JoyDhar32 <joyustcian@gmail.com>
2024-01-22 17:41:13 +1100

    ⭐ Object In Depth

--- JS/Basic/Object.js
+++ JS/Basic/Object.js
@@ -1,6 +1,5 @@
 /*
 In JavaScript, an object is a complex data type that allows you to store and organize data using key-value pairs. Objects are a fundamental part of the language, and they provide a flexible way to represent structured information. Here's an in-depth explanation of JavaScript objects:*/
-
 /*Creating Objects:
 Objects can be created using different syntaxes:*/
 
@@ -15,8 +14,14 @@ const person = {
       State: "NSW",
       country: "Australia",
     },
+    fullname(){
+      return `${this.firstName} ${this.lastName}`;
+    },
   };
-  
-    console.log(person);
-    console.log(person.firstName);
-    console.log(person.lastName);
+  console.log(`Full Name: ${person.firstName} ${person.lastName} \nAge: ${person.age} \nHobbies: ${person.hobbies} \nAddress: ${person.address.city}, ${person.address.State}, ${person.address.country}`);
+  //push property
+  person.email = "joynsw100@gmail.com";
+  person.hobbies.push("Reading Books");
+  console.log(`Email: ${person.email} \nperson.hobbies: ${person.hobbies}`);
+
+  console.log(person.fullname());
