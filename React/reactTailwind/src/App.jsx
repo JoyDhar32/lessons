e8551d4da32ffbb0f641acf531456b9f2cef612f
JoyDhar32 <joyustcian@gmail.com>
2024-01-24 01:28:16 +1100

    Install Tailwind Css

---
+++ React/reactTailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <h1 className="text-3xl color-blue-300 bg-green-700">
+React + Tailwind Css
+ </h1>
+    </>
+  )
+}
+
+export default App
