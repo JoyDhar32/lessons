bb26e383822eca2f9228107aa88bbce1c65dec4a
JoyDhar32 <joyustcian@gmail.com>
2024-01-23 23:57:23 +1100

    Adding Arrow Function No Style

--- React/counter/src/App.jsx
+++ React/counter/src/App.jsx
@@ -1,18 +1,25 @@
-import './App.css';
-import {useState, React} from 'react';
+import "./App.css";
+import { useState, React } from "react";
 
 function App() {
-let [counter, setCounter]= useState(10) // [state, setState
-  let addValue= ()=>{ setCounter(counter=>counter+1) 
-  
-    setCounter(counter=>counter+1)}
+  let [counter, setCounter] = useState(10); // [state, setState
+  let addValue = () => {
+    setCounter((counter) => counter + 1);
+
+    setCounter((counter) => counter + 1);
+  };
   return (
     <div className="App">
-     <h1>React App</h1>
-<button onClick={()=>{setCounter(counter=>counter-1)}}>Subtract(-)</button>
-<h2>Counter Value: {counter}</h2>
-<button onClick={addValue}>Add</button> &nbsp;
-
+      <h1>React App</h1>
+      <button
+        onClick={() => {
+          setCounter((counter) => counter - 1);
+        }}
+      >
+        Subtract(-)
+      </button>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add(+2)</button> &nbsp;
     </div>
   );
 }
