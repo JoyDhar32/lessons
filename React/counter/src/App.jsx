7c396907135e5dc0ac32773a4778a0d0a681e5ca
JoyDhar32 <joyustcian@gmail.com>
2024-01-23 23:52:52 +1100

    adding arrow funciton

--- React/counter/src/App.jsx
+++ React/counter/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css';
-import React from 'react';
+import {useState, React} from 'react';
 
 function App() {
+let [counter, setCounter]= useState(10) // [state, setState
+  let addValue= ()=>{ setCounter(counter=>counter+1) 
+  
+    setCounter(counter=>counter+1)}
   return (
     <div className="App">
       <h1>React App</h1>
+<button onClick={()=>{setCounter(counter=>counter-1)}}>Subtract(-)</button>
+
+<h2>Counter Value: {counter}</h2>
+<button onClick={addValue}>Add</button> &nbsp;
+
     </div>
   );
 }
