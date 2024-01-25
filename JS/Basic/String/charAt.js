81956607f99140a56ec66cbf7b6ef422f239346b
JoyDhar32 <joyustcian@gmail.com>
2024-01-25 15:59:08 +1100

    ⭐ charAt in depth ⭐

---
+++ JS/Basic/String/charAt.js
@@ -0,0 +1,14 @@
+/*
+Character At()
+The charAt() method returns the character at the specified index in a string.
+The index of the first character is 0, the second character is 1, and so on.
+Tip: The index of the last character in a string is string.length-1, the second last character is string.length-2, and so on (See "More Examples").
+*/
+
+let str = "HELLO WORLD";
+console.log(str.charAt(0)); // Returns "H"
+console.log(str.charCodeAt(0)); // Returns 72
+console.log('a'.charCodeAt()); // Returns 97
+console.log(str.at(1))
+
+// ⭐ charAt in depth ⭐
