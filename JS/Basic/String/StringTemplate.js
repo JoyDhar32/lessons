15f3aeebc154feac97bc191e1481a061a550dc95
JoyDhar32 <joyustcian@gmail.com>
2024-01-25 15:38:49 +1100

    ⭐ String Template ⭐

---
+++ JS/Basic/String/StringTemplate.js
@@ -0,0 +1,14 @@
+/*
+String Template: 
+    1. Backtick `` 
+    2. ${}
+
+    */
+
+let Escape =`Hi. My namei "Joy Dhar". I am a "Web Developer".\nI am learning 'JavaScript'.\n I love 'JavaScript'.`;
+console.log(Escape);
+
+//⭐ String Template ⭐
+
+
+
