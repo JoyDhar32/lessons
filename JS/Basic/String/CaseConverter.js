a9f28403a453ba6c6798d6d8bb4708ae73c13511
JoyDhar32 <joyustcian@gmail.com>
2024-01-25 17:03:35 +1100

    ⭐ UpperCase and LowerCase in depth ⭐

--- JS/Basic/String/CaseConverter.js
+++ JS/Basic/String/CaseConverter.js
@@ -7,5 +7,19 @@ console.log(lowerToUpper('hello world!')); // Returns "HELLO WORLD!"
 const upperToLower = (str) => {return str.toLowerCase();}   
 console.log(upperToLower('HELLO WORLD!')); // Returns "hello world!"
 
+// First letter of a string to UpperCase:
+const firstLetterToUpper = (str) => {return str.charAt(0).toUpperCase() + str.slice(1);}
+
+const secondLetterToLower = (str) => {return str.charAt(0)+ str.charAt(1).toLowerCase() + str.slice(2);}
+
+console.log(`Convert First Letter to UpperCase: ${firstLetterToUpper('hello world!')}`); // Returns "Hello world!"
+
+console.log(`Convert Second Letter to LowerCase: ${secondLetterToLower('HEllo World!')}`); // Returns "hELLO WORLD!"
+
+console.log(firstLetterToUpper('hello world!')); // Returns "Hello world!"
+
+console.log(secondLetterToLower('HELLO WORLD!')); // Returns "hELLO WORLD!"
+
+
 // ⭐ UpperCase and LowerCase in depth ⭐
 
