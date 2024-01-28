c13b62525acac896d981c6e7574233a4a87434ff
JoyDhar32 <joyustcian@gmail.com>
2024-01-28 21:47:55 +1100

    ⭐ Array push in depth ⭐

---
+++ JS/Basic/Array/arrayPush.js
@@ -0,0 +1,13 @@
+/*
+Array Push: Add an element to the end of an array
+The push() method adds a new element to an array (at the end), and returns the new length of the array.
+Note: The new item(s) will be added at the end of the array.
+*/
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.push("Kiwi")); // Returns 5
+console.log(fruits); // Returns ["Banana", "Orange", "Apple", "Mango", "Kiwi"]
+let last = fruits.push("Kiwi");
+console.log(last); // Returns 6
+console.log(fruits); // Returns ["Banana", "Orange", "Apple", "Mango", "Kiwi", "Kiwi"]
+
+// ⭐ Array push in depth ⭐
