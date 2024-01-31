e2e84d24a5dfe608a79e9c6566092fb678ad06ee
JoyDhar32 <joyustcian@gmail.com>
2024-02-01 10:27:43 +1100

    Add Bootstrap

--- React/reactHooks/src/App.jsx
+++ React/reactHooks/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import UseStateHooks from "./components/UseStateHooks";
 
--- React/reactHooks/src/components/UseStateHooks.jsx
+++ React/reactHooks/src/components/UseStateHooks.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
+
 const UseStateHooks = () => {
+  let [count, setCount] = useState(0);
+  let [name, setName] = useState(""); // [state, setState
   return (
-    <div>useStateHooks</div>
-  )
-}
-export default UseStateHooks
+    <>
+      <div>useStateHooks</div>
+      <div className="input-group input-group-sm mb-3">
+        <span className="input-group-text">Small</span>
+        <input
+          type="text"
+          className="form-control"
+          plceholder="enter your name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        {" "}
+        <span style={{ color: "green", fontWeight: "bold" }}>
+          {name}{" "}
+        </span>click {count >= 0 ? count : 0} Times
+      </div>
+      <button onClick={() => setCount((prev) => prev + 1)} className="btn btn-sm btn-primary mt-3">Increment</button>
+      &nbsp; &nbsp;
+      <button onClick={() => setCount(() => count - 1)} disabled={count <= 0} className="btn btn-sm btn-warning mt-3">
+        Decrement
+      </button>
+    </>
+  );
+};
+export default UseStateHooks;
