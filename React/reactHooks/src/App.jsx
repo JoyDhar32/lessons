f97780ebd524fad6e182802f57cd11a2ccabfbf7
JoyDhar32 <joyustcian@gmail.com>
2024-02-01 10:03:37 +1100

    useState

--- React/reactHooks/src/App.jsx
+++ React/reactHooks/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
+import UseStateHooks from "./components/UseStateHooks";
 
 function App() {
   return (
     <>
       <h1>Welcome to React Hook</h1>
+      <UseStateHooks />
     </>
   );
 }
---
+++ React/reactHooks/src/components/UseStateHooks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const UseStateHooks = () => {
+  return (
+    <div>useStateHooks</div>
+  )
+}
+export default UseStateHooks
