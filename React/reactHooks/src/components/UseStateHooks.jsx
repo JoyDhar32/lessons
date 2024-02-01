1f4a1f48eb7798eae7a8dc902bb72175a9d830de
JoyDhar32 <joyustcian@gmail.com>
2024-02-01 16:10:21 +1100

    ⭐️ useState Hook ⭐️

--- React/reactHooks/src/components/UseStateHooks.jsx
+++ React/reactHooks/src/components/UseStateHooks.jsx
@@ -39,3 +39,12 @@ const UseStateHooks = () => {
   );
 };
 export default UseStateHooks;
+/*
+UseStateHooks: 
+- useState is a Hook that allows you to have state variables in functional components.
+- useState returns a pair: the current state value and a function that lets you update it.
+- useState is a new way to use the exact same capabilities that this.state provides in a class.
+- useState is a Hook (we’ll talk about what this means in a moment). We call it inside a function component to add some local state to it.
+- React will preserve this state between re-renders.
+⭐️ useState Hook ⭐️
+*/
