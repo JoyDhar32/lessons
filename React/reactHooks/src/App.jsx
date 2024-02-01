b7c26e4be593684bec7de33bd191910662aa0bec
JoyDhar32 <joyustcian@gmail.com>
2024-02-01 16:14:15 +1100

    useefferct

--- React/reactHooks/src/App.jsx
+++ React/reactHooks/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import UseEffectHook from "./components/UseEffectHook";
 import UseStateHooks from "./components/UseStateHooks";
+
 
 function App() {
   return (
     <>
       <h1>Welcome to React Hook</h1>
-      <UseStateHooks />
+      {/* <UseStateHooks /> */}
+      <UseEffectHook />
     </>
   );
 }
---
+++ React/reactHooks/src/components/UseEffectHook.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UseEffectHook = () => {
+  return (
+    <div>UseEffectHook</div>
+  )
+}
+
+export default UseEffectHook
+
+/*
+variation of useEffect Hook
+- useEffect Hook runs after every render of the component.
+
+*/
