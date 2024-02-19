c98d101434678ea55c91ad818a85dcaf86abd126
JoyDhar32 <joyustcian@gmail.com>
2024-02-19 13:22:17 +1100

    ⭐ Comparision Operator ⭐

---
+++ JS/Basic Lession/StrictOperator.js
@@ -0,0 +1,26 @@
+// assignment operator
+let x = 10;
+let y = 20;
+console.log(x, y);
+
+// comparison operator
+let a = 10;
+let b = 20;
+console.log(a == b); // false
+console.log(a === b); // false 
+console.log(a != b); // true
+console.log(a !== b); // true
+
+// strict equality operator
+let c = 10;
+let d = "10";
+console.log(c == d); // true
+console.log(c === d); // false
+
+// strict inequality operator
+let e = 10;
+let f = "10";
+console.log(e != f); // false
+console.log(e !== f); // true
+
+// ⭐ Comparision Operator ⭐
