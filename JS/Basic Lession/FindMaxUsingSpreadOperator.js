7f70eca07d5f94a1b76f31116e5d6f0fdaddf84d
JoyDhar32 <joyustcian@gmail.com>
2024-02-20 12:24:44 +1100

    ⭐ Includes Operator ⭐

--- JS/Basic Lession/FindMaxUsingSpreadOperator.js
+++ JS/Basic Lession/FindMaxUsingSpreadOperator.js
@@ -5,3 +5,4 @@ console.log(finMax([1,2,3,4,5,6,7,8,9,10])); // 10
 console.log(finMax([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]));
 console.log(finMax([1,2,3,4,5,6,7,8,9,1000,11,12,13,14,15,100]));
 
+//⭐ Find Max Using Spread Operator ⭐
