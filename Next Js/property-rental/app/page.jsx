0f9a1935b6f688ab4fd59beb0078f0c19a0c0787
JoyDhar32 <joyustcian@gmail.com>
2024-02-24 00:03:09 +1100

    update

--- Next Js/property-rental/app/page.jsx
+++ Next Js/property-rental/app/page.jsx
@@ -1,10 +1,12 @@
+'use client'
 import React from 'react'
 
-export const metadata ={
-title: 'Home Page'
-}
+// export const metadata ={
+// title: 'Home Page'
+// }
 
 const page = () => {
+    console.log('MainLayout')
   return (
     <div className="mt-4 text-red-200">page</div>
   )
