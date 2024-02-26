3456786bdc062bd5dcde17db1bacdc2743b659cd
JoyDhar32 <joyustcian@gmail.com>
2024-02-26 19:49:34 +1100

    Adding Profile

--- React/reactRouterDom/src/Pages/Profile.jsx
+++ React/reactRouterDom/src/Pages/Profile.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
-const params=useParams();
+  const params = useParams();
   return (
     <div className="container">
-        <h1>Profile- {params.profileId}</h1>
-        <p>
-            Welcome to the profile page! This is a simple example of a React component.
-        </p>
+      <h1>Profile- {params.profileId}</h1>
+      <p>
+        Welcome to the profile page! This is a simple example of a React
+        component.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
