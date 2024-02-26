eb1effe284af698688a607a2245b9c1fa6a67e26
JoyDhar32 <joyustcian@gmail.com>
2024-02-26 13:30:52 +1100

    adding Pages

---
+++ React/reactRouterDom/src/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>    
+    <div className="container ">
+        <h1>Home Page</h1>
+        <p>
+            Welcome to the home page! This is a simple example of a React component.
+        </p>
+    </div>
+    </>
+
+  )
+}
+
+export default HomePage
