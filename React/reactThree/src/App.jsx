61fc2e50a4779a4aa4d652e5d93fc05cea45f456
JoyDhar32 <joyustcian@gmail.com>
2024-03-06 10:47:54 +1100

    adding three js

--- React/reactThree/src/App.jsx
+++ React/reactThree/src/App.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
-
+import {useEffect} from 'react'
+import * as THREE from 'three'
 const App = () => {
+  useEffect (()=>{
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+    camera.position.z=96;
+    const canvas = document.getElementById('myThreeJsCanvas');
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Lighting
+
+const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
+ambientLight.castShadow = true;
+scene.add(ambientLight);
+
+const spotLight = new THREE.SpotLight(0xffffff, 1);
+spotLight.castShadow = true;
+spotLight.position.set(0,64,32);
+
+const animate=()=>{
+  renderer.render(scene,camera);
+  window.requestAnimationFrame(animate);
+};
+animate();
+  },[])
   return (
-    <div className="text-bold text-red-500">App</div>
+    <>
+    <div>
+      <canvas id="myThreeJsCanvas" />
+
+    </div>
+    </>
   )
 }
 
