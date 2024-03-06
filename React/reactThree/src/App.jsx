8c692611016b686104324e5dc3b60b8c20d2b648
JoyDhar32 <joyustcian@gmail.com>
2024-03-06 19:21:51 +1100

    Initial commit

--- React/reactThree/src/App.jsx
+++ React/reactThree/src/App.jsx
@@ -1,46 +1,10 @@
 import React from 'react'
-import {useEffect} from 'react'
-import * as THREE from 'three'
+import BoxControll from './components/BoxControll'
+
 const App = () => {
-  useEffect (()=>{
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z=96;
-    const canvas = document.getElementById('myThreeJsCanvas');
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-    });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-//Lighting
-
-const ambientLight = new THREE.AmbientLight(0xffffff,0.5);
-ambientLight.castShadow = true;
-scene.add(ambientLight);
-
-const spotLight = new THREE.SpotLight(0xffffff, 1);
-spotLight.castShadow = true;
-spotLight.position.set(0,64,32);
-
-const animate=()=>{
-  renderer.render(scene,camera);
-  window.requestAnimationFrame(animate);
-};
-animate();
-  },[])
   return (
     <>
-    <div>
-      <canvas id="myThreeJsCanvas" />
-
-    </div>
+    <BoxControll />
     </>
   )
 }
---
+++ React/reactThreeFiberMix/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className="font-bold text-purple-900 text-2xl text-center pt-4">React Three Fiber Mix</h1>
+    </>
+  )
+}
+
+export default App
