8ed8f788ebfdc2d7955bf171eeeeab8e5c309e42
JoyDhar32 <joyustcian@gmail.com>
2024-03-17 20:59:59 +1100

    importing Component

---
+++ Next Js/basic/src/app/profile/Joy.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function joy() {
+  return (
+    <div>joy</div>
+  )
+}
--- Next Js/basic/src/app/profile/page.tsx
+++ Next Js/basic/src/app/profile/page.tsx
@@ -1,7 +1,10 @@
+
+import Joy from './Joy.tsx'
 export default function page() {
   return (
     <div>
         <h1 className="text-3xl text-red-500"> My Profile </h1>
+        <Joy />
     </div>
   )
 }
