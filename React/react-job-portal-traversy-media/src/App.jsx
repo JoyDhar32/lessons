d2427205db6e67ae6244e08de1975f630298a5c8
JoyDhar32 <joyustcian@gmail.com>
2024-03-22 22:06:21 +1100

    adding expressions

--- React/react-job-portal-traversy-media/src/App.jsx
+++ React/react-job-portal-traversy-media/src/App.jsx
@@ -2,10 +2,23 @@ import React from "react";
 
 const App = () => {
   let name="Joy Dhar"
+  let x = 10; 
+  let y = 20;
+  let arr=['a','b','c','d']
   return (
     <>
       <div className="text-5xl text-red-500">App</div>
       <div className="text-2xl text-purple-500">{name}</div>
+      {
+        x > y ? <div className="text-2xl text-green-500">{x} is greater than {y}</div> : <div className="text-2xl text-green-500">{y} is greater than {x}</div>
+      }
+      <ul>
+        {
+          arr.map((item,index)=>{
+            return <li key={index}>{item}</li>
+          })
+        }
+      </ul>   
     </>
   );
 };
