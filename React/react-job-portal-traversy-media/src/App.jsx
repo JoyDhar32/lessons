6e84b481b110da4e6d4831e30c5081cd8cd1f09a
JoyDhar32 <joyustcian@gmail.com>
2024-03-22 22:00:26 +1100

    adding tailwind css

--- React/react-job-portal-traversy-media/src/App.jsx
+++ React/react-job-portal-traversy-media/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 const App = () => {
+  let name="Joy Dhar"
   return (
     <>
       <div className="text-5xl text-red-500">App</div>
+      <div className="text-2xl text-purple-500">{name}</div>
     </>
   );
 };
