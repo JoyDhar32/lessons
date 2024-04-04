4c2da42cb689ced247a4803e81696782fcc78dd7
JoyDhar32 <joyustcian@gmail.com>
2024-04-05 00:14:22 +1100

    update

---
+++ ThreeJs/fundamentalThreeJS/js/script.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const texterLoader= new THREE.TextureLoader();
+const orbit = new OrbitControls(camera, renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color:'gray',  map: texterLoader.load('https://threejs.org/examples/textures/crate.gif')});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+orbit.update();
+
+const animate = function () {
+	requestAnimationFrame(animate);
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render(scene, camera);
+};
+
+animate();
