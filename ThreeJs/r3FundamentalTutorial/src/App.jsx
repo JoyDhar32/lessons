feadc7b143d0153d060b46ba84f20c019f095602
JoyDhar32 <joyustcian@gmail.com>
2024-04-05 01:09:03 +1100

    update

--- ThreeJs/r3FundamentalTutorial/src/App.jsx
+++ ThreeJs/r3FundamentalTutorial/src/App.jsx
@@ -1,28 +1,27 @@
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import "./App.css";
-const App = () => {
-  const Cube=({position, side, color})=>{
-    return(
+
+const Cube = ({ position, side, color }) => {
+  return (
     <mesh position={position}>
-    <boxGeometry args={side}/>
-    <meshStandardMaterial color={color} />
-  </mesh>
-    )
-  }
+      <boxGeometry args={side} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+const App = () => {
   return (
     <>
       <Canvas>
-      <ambientLight intensity={0.5} />
-        <directionalLight position={[0,0 , 1]} intensity={0.5}/> 
-        <group position={[0,-1,0]}> 
-
-        <Cube position={[-1,0,0]} side={[1,1,1]} color={"red"} />
-        <Cube position={[1,0,0]} side={[1,1,1]} color={"hotpink"} />
-        <Cube position={[-1,2,0]} side={[1,1,1]} color={"blue"} />
-        <Cube position={[1,2,0]} side={[1,1,1]} color={"orange"} />
-</group>
-
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 0, 1]} intensity={0.5} />
+        <group position={[0, -1, 0]}>
+          <Cube position={[-1, 0, 0]} side={[1, 1, 1]} color={"red"} />
+          <Cube position={[1, 0, 0]} side={[1, 1, 1]} color={"hotpink"} />
+          <Cube position={[-1, 2, 0]} side={[1, 1, 1]} color={"blue"} />
+          <Cube position={[1, 2, 0]} side={[1, 1, 1]} color={"orange"} />
+        </group>
       </Canvas>
     </>
   );
