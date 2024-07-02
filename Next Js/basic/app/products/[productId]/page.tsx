fef9ab7d1374defb26da1f03f6157fb015b16ccc
JoyDhar32 <joyustcian@gmail.com>
2024-07-03 02:02:19 +1000

    custom 404 page

--- Next Js/basic/app/products/[productId]/page.tsx
+++ Next Js/basic/app/products/[productId]/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { notFound } from "next/navigation"
+const page = ({ params }: { params: { productId: string } } )  => {
+    {
+        if (parseInt(params.productId) > 5) {
+            return notFound()
+        }
+    }
+  
+    return (
+        <>
+            <h1>Product Id: {params.productId}</h1>
+            { }
 
-const page = ({params}:{params:{productId:number}}) => {
-  return (
-    <>
-    <h1>Product Id: {params.productId}</h1>
-    </>
-  )
+        </>
+    )
 }
 
 export default page
