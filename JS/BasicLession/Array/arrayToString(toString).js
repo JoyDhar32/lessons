010071085fbcfc6eb189bb75b7fecf9849963d11
JoyDhar32 <joyustcian@gmail.com>
2024-07-08 16:53:28 +1000

    ⭐ To String Function ⭐

---
+++ JS/BasicLession/Array/arrayToString(toString).js
@@ -0,0 +1,19 @@
+const arr=[1,2,3,4,5,6,7,8,9,10];
+const str=arr.toString(); // "1,2,3,4,5,6,7,8,9,10"
+console.log(str);
+console.log(arr)
+
+console.log(`-----------------`)
+const arr2=['a','aa','b','bb','c','cc','d','dd','e','ee'];
+const str2=arr2.toString();
+console.log(arr2);
+console.log(str2);
+
+console.log(`-----------------`)
+
+console.log(`The type of arr is ${typeof arr}`);
+console.log(`The type of Str is ${typeof str}`);
+console.log(`The type of arr2 is ${typeof arr2}`);
+console.log(`The type of Str2 is ${typeof str2}`);
+
+// ⭐ To String Function ⭐
